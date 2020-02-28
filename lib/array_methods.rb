caaef19c248d88ed16a157d1b1a512e4b602033a
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 05:14:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,28 +1,13 @@
 def find_element_index(array, value_to_find)
-  array.find each do 
+  array.index each do |array|
+    puts array.index("#{value}")
 end
-
-
-describe 'find_element_index(array, value_to_find)' do
-    it 'takes in an array and a value and returns the index of that value' do
-      expect(find_element_index(scale,2)).to eq(0)
-      expect(find_element_index(scale,4)).to eq(2)
-      expect(find_element_index(scale_2,5)).to eq(1)
-      expect(find_element_index(scale_2,6)).to eq(0)
-    end
-
-    it 'if the value is not found, returns nil' do
-      expect(find_element_index(hill,500)).to eq(nil)
-      expect(find_element_index(valley,500)).to eq(nil)
-      expect(find_element_index(zig_zag,400)).to eq(nil)
-    end
 
 def find_max_value(array)
   
 array.max_by do |element|
  return element
-  
-end
+
 end
 
 def find_min_value(array)
