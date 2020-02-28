3f0ff06ceea31ceec1132b54db2cbd9313ee0e6f
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-28 05:44:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.find_index { |array, value| }
-   
+  array.find_index do |array, value| 
+   puts index
+ end
 end
  
 def find_max_value(array)
